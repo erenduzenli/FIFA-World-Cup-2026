7494c2ab6fe03429ae2b8a67f76d41822959d16f
erenduzenli <159820915+erenduzenli@users.noreply.github.com>
2026-05-29 14:31:52 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "FIFA Dünya Kupası 2026",
   description: "Dünya Kupası oyun sitesi",
+  icons: {
+    icon: "/world-cup-logo.png",
+    shortcut: "/world-cup-logo.png",
+    apple: "/world-cup-logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
