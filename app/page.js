462105463ead998bfb36f86c7c420b82aa25ebb1
erenduzenli <159820915+erenduzenli@users.noreply.github.com>
2026-07-13 11:14:03 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1604,12 +1604,12 @@ ctx.fillText("Şampiyonlar", width / 2, 188);
 const centerY = y + 58;
 
 // Sıra / madalya
-ctx.font = "900 46px Arial";
+ctx.font = "900 34px Arial";
 ctx.fillStyle = medalColors[index];
 ctx.textAlign = "left";
 ctx.fillText(medals[index], 150, centerY);
 
-ctx.font = "900 30px Arial";
+ctx.font = "900 34px Arial";
 ctx.fillText(`${index + 1}.`, 220, centerY);
 
 // İsim
@@ -1620,8 +1620,9 @@ ctx.fillText(player.name, 300, centerY);
 // Puan
 ctx.textAlign = "right";
 ctx.fillStyle = medalColors[index];
-ctx.font = "900 42px Arial";
+ctx.font = "900 34px Arial";
 ctx.fillText(String(player.points), 1030, centerY);
+
 
 ctx.textAlign = "left";
   });
